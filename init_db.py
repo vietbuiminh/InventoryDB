ac797e978825af5f8b70861c761ac806caf2c0a7
Viet Bui <buiminhviet.2002@gmail.com>
2024-06-26 20:09:19 +0000

    added reference sql to implement the same system

--- init_db.py
+++ init_db.py
@@ -2,52 +2,59 @@ import sqlite3
 
 connection = sqlite3.connect('database.db')
 with open('schema.sql') as f:
-    connection.executescript(f.read())
+  connection.executescript(f.read())
 
 cur = connection.cursor()
+cur.connection.execute("INSERT INTO category (category_name) VALUES (?)",
+                       ('test1', ))
+connection.commit()
 
 
 class Category:
-    id = 0
-    category_name = ""
+  id = 0
+  category_name = ""
 
-    def __init__(self, category_name):
-        self.id = id
-        self.category_name = category_name
+  def __init__(self, category_name):
+    self.category_name = category_name
 
-    def get_id(self):
-        return self.id
+  def create(self):
+    if cur.connection.execute("SELECT * FROM category WHERE category_name = ?",
+                              (self.category_name, )).fetchone() == []:
+      cur.connection.execute("INSERT INTO category (category_name) VALUES (?)",
+                             (self.category_name, ))
+      self.id = connection.execute(
+          "SELECT id FROM category WHERE category_name = ?",
+          (self.category_name, )).fetchone()[0]
+      connection.commit()
+      print('created')
+      return True
+    else:
+      return False
 
-    def get_name(self):
-        return self.category_name
+  def get_id(self):
+    sql = 'SELECT id FROM category WHERE category_name = ?'
+    val = (self.category_name, )
+    self.id = cur.connection.execute(sql, val).fetchone()[0]
+    return self.id
 
-    def create(self):
-        if cur.connection.execute("SELECT * FROM category WHERE category_name = ?",
-                                  (self.category_name, )).fetchone() == []:
-            cur.connection.execute("INSERT INTO category (category_name) VALUES (?)",
-                                   (self.category_name, ))
-            self.id = cur.connection.execute(
-                "SELECT id FROM category WHERE category_name = ?",
-                (self.category_name, )).fetchone()[0]
-            connection.commit()
-            return True
-        else:
-            return False
+  def get_name(self):
+    return self.category_name
 
-    def delete(self):
-        cur.connection.execute(
-            "DELETE FROM category WHERE id = ?", (self.id, ))
-        connection.commit()
+  def delete(self):
+    cur.connection.execute("DELETE FROM category WHERE category_name = ?",
+                           (self.category_name, ))
+    connection.commit()
+    print('deleted')
 
-    def __str__(self):
-        return f"{self.id}. {self.category_name}"
+  def __str__(self):
+    return f"{self.id}. {self.category_name}"
 
-    def __repr__(self):
-        return f"<Category {self.category_name}>"
+  def __repr__(self):
+    return f"<Category {self.category_name}>"
 
 
 general_type = Category('general')
 general_type.create()
-print(general_type.get_name())
+
 connection.commit()
 connection.close()
